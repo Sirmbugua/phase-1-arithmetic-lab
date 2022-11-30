cbdd78b785db86f54a1a49976b45583e85b89b95
Teddy <teddy.mbugua@student.moringaschool.com>
2022-11-30 19:47:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+var multiply;
+var num1 = 31;
+var num2 = 2;
+multiply = num1*num2;
+
+var random = 0;
+random++;
+
+var mod ;
+var num3 =94;
+var num4 = 5 ;
+mod = num3%num4;
+
+
+
+var max;
+max =20;
